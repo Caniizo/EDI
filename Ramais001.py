e2a2ea68a65cbf42f3ad9f642a67c32797e53ecc
Caniizo <103270512+Caniizo@users.noreply.github.com>
2022-07-29 12:37:30 -0300

    ersa

---
+++ Ramais001.py
@@ -0,0 +1,23 @@
+logistica = ["Vaz"]
+logisticaRamais = [3192]
+
+ti = ["Bruno", "Eduardo", "Felipe", "Idevaldo", "Julian", "Laura", "Rafael", "Rizzi", "Vinicius"]
+tiRamais = [3428, 3018, 3619, 3418, 3419, 3620, 3618, 3019, 3263]
+
+y = 0
+
+print ("Logistica")
+for x in logistica:
+    numeroCompleto = "Nada"
+    numeroCompleto = str(logistica[y]) + str (" ") + str(logisticaRamais[y])
+    print (numeroCompleto)
+    y += 1
+
+y = 0
+
+print ("TI")
+for x in ti:
+    numeroCompleto = "Nada"
+    numeroCompleto = str(ti[y]) + str (" ") + str(tiRamais[y])
+    print (numeroCompleto)
+    y += 1
